64a363c87d70f666b9a86e2c106bfd69c7f2871a
Ricardo Campos <ricardompcampos@gmail.com>
2026-04-03 18:34:45 -0300

    Add S3 backend configuration to main.tf
    Configure S3 backend for Terraform state management.

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,18 @@ terraform {
       version = ">= 2.0.0"
     }
   }
+
+  backend "s3" {
+    bucket                      = "gif-mood"
+    key                         = "kubernetes/terraform.tfstate"
+    region                      = "auto"
+    endpoints                   = { s3 = "https://d17eb09b6bce2f90e16e800bb2a6baf9.r2.cloudflarestorage.com" }
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_s3_checksum            = true
+  }
 }
 
 provider "kubernetes" {
